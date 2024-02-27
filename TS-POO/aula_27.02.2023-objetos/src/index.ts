3706506907aad1389674d3288544b6771f84b637
Leticia Leal <leticialealp@hotmail.com>
2024-02-27 18:26:23 -0300

    setup aula 27.02

---
+++ TS-POO/aula_27.02.2023-objetos/src/index.ts
@@ -0,0 +1,87 @@
+
+// VARIÁVEIS estruturadas
+
+/*
+
+    OBJETO (literal)
+    Usado sempre que precisarmos armazenar vários dados de tipos diferentes referente à uma mesma coisa.
+    Ex: dados de um aluno, dados de um produto, dados de uma assinatura, dados de um pedido, etc
+
+    Podem ser:
+    1 - Anônimos
+    2 - Nomeados 
+
+*/
+
+// Tipos anônimos de objetos literais 
+
+
+
+// Tipos nomeados de objetos literais - INTERFACE
+
+
+// Tipos nomeados de objetos literais - TYPE ALIAS
+
+
+
+// Propriedades opcionais em um objeto - MARK ?
+
+
+
+
+
+/* 
+    TYPE x INTERFACE
+
+    Type
+    - Pode ser utilizado como alias para outras tipagens. Ou seja, construir tipos customizados a partir de outros tipos
+
+    Interfaces
+    - Pode-se criar novas interfaces que se unem à outras interfaces utilizando o termo "extends"
+
+*/
+
+// TYPE ALIASES - Caso de uso: quando precisamos de valores pre determinados em uma variável
+
+
+
+// União de Tipos (INTERSECTION TYPES) - criar um novo tipo a partir da união de outros 2 ou mais tipos
+// O novo tipo criado utilizando a União (&) terá as propriedades de ambos os tipos
+
+
+
+// Intersecção de Tipos (UNION TYPES) - criar um novo tipo que tenha apenas as propriedades comuns entre 2 ou mais tipos
+// Um tipo pode ser composto por duas opções de forma exclusiva (ou um, ou outro).
+
+
+
+// União de Interfaces (extends) - Interfaces podem se unir a outras interfaces do mesmo jeito que a união dos types
+// OBS:  Uma interface pode extender apenas 1 interface
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
