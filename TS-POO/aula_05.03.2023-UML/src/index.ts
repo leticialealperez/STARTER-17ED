3e66a285f8c7bd5e5a6e50771c4b9e7f624213c0
Leticia Leal <leticialealp@hotmail.com>
2024-03-05 18:09:03 -0300

    setup aula 05.03.2024

---
+++ TS-POO/aula_05.03.2023-UML/src/index.ts
@@ -0,0 +1,14 @@
+/*
+    RELACIONAMENTOS ENTRE CLASSES
+    1 - Herança    - Cria um relacionamento onde "X" é "Y"
+    2 - Composição - Onde a classe que está contida na relação não pode existir sem a classe que a contém.
+    3 - Associação - Onde é possível que uma classe exista mesmo que sua associação seja excluída.
+    4 - Agregação  - Seu objetivo é mostrar que uma classe faz uso de outra para determinada ação.
+
+
+
+    MULTIPLICIDADE
+    Expressa a quantidade de ocorrências de uma classe em outra.
+    Ajuda a entender se é um array ou um atributo único que a classe irá conter, além de entender se será opcional ou obrigatório.
+
+*/
