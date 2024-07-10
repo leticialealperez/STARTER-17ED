3b867ae2f6f71354a0439a9a5e2feff08197237a
Leticia Leal <leticialealp@hotmail.com>
2024-07-09 22:30:28 -0300

    add a chamada para o tema padrão e estilos globais

--- PROJETO-BLOCO-INTERMEDIARIO/setup-growtwitter-app/src/App.tsx
+++ PROJETO-BLOCO-INTERMEDIARIO/setup-growtwitter-app/src/App.tsx
@@ -1,10 +1,13 @@
+import { GlobalStyles } from './configs/global/GlobalStyles';
+import { LightTheme } from './configs/theme/LightTheme';
 import { AppRoutes } from './routes/AppRoutes';
 
 
 export function App() {
   return (
-    <>
+    <LightTheme>
+      <GlobalStyles />
       <AppRoutes />
-    </>
+    </LightTheme>
   )
 }
